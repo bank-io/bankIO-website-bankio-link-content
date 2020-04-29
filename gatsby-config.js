f3a80f48ed0c426be957fa209002d71fd9f9c336
Florin Uifelean <florin@bankio.co.uk>
2020-04-29 14:27:16 +0300

    init

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://bankio.co.uk/openbanking/',
+    title: 'bankIO Link documentation',
+    description:
+      'Test the PSD2/OpenBanking APIs offered by ASPSPs'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `openbanking-content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    // {
+    //   // resolve: '@stackbit/gatsby-plugin-menus',
+    //   resolve: 'gatsby-plugin-menus',
+    //   options: {
+    //     // static definition of menu items (optional)
+    //     menus: {
+    //       main: // identifier of menu container
+    //         [
+    //         ]
+    //     },
+    //     // Gatsby node types from which we extract menus (optional, see "Advanced usage")
+    //     sourceNodeType: 'Mdx', 
+    //     // the relative node path where we can find the 'menus' container (optional)
+    //     sourceDataPath: 'frontmatter',
+    //     // the relative node path where we can find the page's URL (required)
+    //     sourceUrlPath: 'fields.slug',
+    //     // custom menu loading function (optional)
+    //     // menuLoader: customLoaderFunction,
+    //     // the property to use for injecting to the page context (optional, see "Advanced usage")
+    //     // pageContextProperty: 'menus',
+    //   },
+    // },
+  ]
+};
